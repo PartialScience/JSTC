848353a263459e970e755b5cfa1795fe689ccf34
Partial Science <77656980+PartialScience@users.noreply.github.com>
2025-12-26 20:25:32 -0500

    Fix typo in comment

--- .devcontainer/scripts/PostCreationScript.sh
+++ .devcontainer/scripts/PostCreationScript.sh
@@ -3,7 +3,7 @@
 # Install API Python Dependencies
 pip install --no-cache-dir -r /JSTC/api/requirements.txt
 
-# Install pymfem with MPI, GPU, GPU-Hypre, GSLIB, SuiteSparse, libCEED and LAPACK
+# Build pymfem from source and install with parallel and gslib
 apt-get update && apt-get install -y build-essential g++ libopenmpi-dev openmpi-bin swig
 git clone https://github.com/mfem/PyMFEM.git /pymfem_installation
 cd /pymfem_installation 
